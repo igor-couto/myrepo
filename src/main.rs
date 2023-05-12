ffd63ea2ef6a653ee5ef698ff4255ae70e81ef0e
igor-couto <igor.fcouto@gmail.com>
2023-05-12 20:50:21 -0300

    refactor: remove colored package dependency

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
 use std::time::Duration;
 use ureq;
-use colored::*;
 
 fn main() {
     let timeout_seconds = 5;
@@ -38,7 +37,7 @@ fn main() {
             let repo_name = full_name.split('/').nth(1).unwrap_or("");
 
             if substring_to_find.map_or(true, |substring| repo_name.contains(substring)) {
-                println!("https://github.com/{}/{}", user_name, repo_name.green());
+                println!("https://github.com/{}/\x1b[0;32m  {}\x1b[0m", user_name, repo_name);
                 found = true;
             }
             current_start = end;
@@ -48,6 +47,6 @@ fn main() {
     }
 
     if !found {
-        println!("Did not find any repository containing '{}'", substring_to_find.unwrap().red());
+        println!("Did not find any repository containing '\x1b[0;31m{}\x1b[0m'", substring_to_find.unwrap());
     }
 }
