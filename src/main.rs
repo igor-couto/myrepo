8ec95ca1bd45370ebac37b80f1e2c9233b9ff76a
igor-couto <igor.fcouto@gmail.com>
2023-05-12 19:58:34 -0300

    feat: now it is possible to use the CLI without passing any parameters, returning all repositories

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ fn main() {
     let user_name = "igor-couto";
 
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
+    if args.len() > 2 {
         eprintln!("Usage: {} <substring>", args[0]);
         std::process::exit(1);
     }
-    let substring_to_find = &args[1];
+    let substring_to_find = args.get(1);
 
     let mut page = 1;
+    let mut found = false;
 
     loop {
         let response : String = ureq::get(&format!("https://api.github.com/users/{}/repos?page={}&per_page=100", user_name, page))
@@ -30,24 +31,23 @@ fn main() {
         }
 
         let mut current_start = 0;
-        let mut found = false;
         while let Some(start_index) = response[current_start..].find("\"full_name\":\"") {
             let start = current_start + start_index + "\"full_name\":\"".len();
             let end = &response[start..].find("\",").unwrap() + start;
             let full_name = &response[start..end];
             let repo_name = full_name.split('/').nth(1).unwrap_or("");
 
-            if repo_name.contains(substring_to_find) {
+            if substring_to_find.map_or(true, |substring| repo_name.contains(substring)) {
                 println!("https://github.com/{}/{}", user_name, repo_name.green());
                 found = true;
             }
             current_start = end;
         }
 
-        if !found {
-            println!("Did not find any repository containing '{}'", substring_to_find.red());
-        }
-
         page += 1;
+    }
+
+    if !found {
+        println!("Did not find any repository containing '{}'", substring_to_find.unwrap().red());
     }
 }
