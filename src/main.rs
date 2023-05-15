a1f88164ed2cab146adf8087dc7dddc144ccffcb
igor-couto <igor.fcouto@gmail.com>
2023-05-15 14:51:48 -0300

    feat: add user configuration for windows environment

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,20 @@ use ureq;
 fn main() {
     let args = parse_arguments(env::args().collect());
 
+    let username = match std::env::var("MYREPO_GITHUB_USERNAME") {
+        Ok(value) => value,
+        Err(_error) => {
+            eprintln!("\x1b[0;31merror:\x1b[0m myrepo does not know what your username is. Please run myrepo -u USERNAME before using it ");
+            std::process::exit(0);
+        }
+    };
+    
     let timeout_seconds = 5;
-    let user_name = "igor-couto";
-
     let mut page = 1;
     let mut found = false;
 
     loop {
-        let response = make_request(user_name, page, timeout_seconds)
+        let response = make_request(username.as_str(), page, timeout_seconds)
             .expect("\x1b[0;31merror:\x1b[0m Some kind of unexpected error has occurred");
 
         if response == "[]" {
@@ -29,10 +35,10 @@ fn main() {
                 .map_or(true, |substring| name.contains(substring))
             {
                 found = true;
-                println!("https://github.com/{}/\x1b[0;32m{}\x1b[0m", user_name, name);
+                println!("https://github.com/{}/\x1b[0;32m{}\x1b[0m", username, name);
 
                 if args.clone_repos {
-                    clone_repository(user_name, &name);
+                    clone_repository(username.as_str(), &name);
                 }
             }
         }
@@ -56,16 +62,54 @@ struct Args {
 fn parse_arguments(mut args: Vec<String>) -> Args {
     args.remove(0);
 
-    let clone_repos = args.contains(&"-c".to_string()) || args.contains(&"--clone".to_string());
+    let mut clone_repos = false;
+    let mut substring_to_find = None;
+    let mut username = None;
 
-    let substring_to_find = args
-        .iter()
-        .find(|&arg| arg != "-c" && arg != "--clone")
-        .cloned();
+    let mut iter = args.iter();
+    while let Some(arg) = iter.next() {
+        match arg.as_str() {
+            "-c" | "--clone" => {
+                if clone_repos {
+                    eprintln!("\x1b[0;31merror:\x1b[0m Duplicate argument: {}", arg);
+                    print_usage_and_exit();
+                }
+                clone_repos = true;
+            }
+            "-u" | "-user" => {
+                match iter.next() {
+                    Some(user) => {
+                        if username.is_some() {
+                            eprintln!("\x1b[0;31merror:\x1b[0m Duplicate argument: {}", arg);
+                            print_usage_and_exit();
+                        }
+                        username = Some(user.clone());
+                    }
+                    None => {
+                        eprintln!("\x1b[0;31merror:\x1b[0m Expected username after {}", arg);
+                        print_usage_and_exit();
+                    }
+                }
+            }
+            _ => {
+                if substring_to_find.is_some() {
+                    eprintln!("\x1b[0;31merror:\x1b[0m Unexpected argument: {}", arg);
+                    print_usage_and_exit();
+                }
+                substring_to_find = Some(arg.clone());
+            }
+        }
+    }
 
-    if args.len() > 2 || args.len() == 2 && !clone_repos {
-        eprintln!("\x1b[0;31merror:\x1b[0m Invalid arguments");
-        eprintln!("Usage: <text> [-c | --clone]");
+    if let Some(username) = username {
+        match set_username_windows(&username) {
+            Ok(_) => {
+                println!("The user {} has been defined successfully", username);
+            },
+            Err(e) => {
+                eprintln!("Failed to set the environment variable: {}", e);
+            }
+        }
         std::process::exit(0);
     }
 
@@ -73,6 +117,27 @@ fn parse_arguments(mut args: Vec<String>) -> Args {
         clone_repos,
         substring_to_find,
     }
+}
+
+fn print_usage_and_exit() {
+    eprintln!("Usage: <text> [-c | --clone] [-u | --user <username>]");
+    std::process::exit(0);
+}
+
+fn set_username_windows(username: &str) -> std::io::Result<()> {
+    let output = Command::new("setx")
+        .arg("MYREPO_GITHUB_USERNAME")
+        .arg(username)
+        .output()?;
+
+    if !output.status.success() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Failed to set environment variable",
+        ));
+    }
+
+    Ok(())
 }
 
 fn make_request(
